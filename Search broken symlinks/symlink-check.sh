45e684f6b5321854021315923e0ad0133a93b003
Marvin Sanders <marvin@sanders-millingen.nl>
2023-08-02 17:17:14 +0200

    Shows now only the Redline or Greenlines.

--- Search broken symlinks/symlink-check.sh
+++ Search broken symlinks/symlink-check.sh
@@ -14,14 +14,19 @@ fi
 # directory location that you want to scan
 scan_dir=$1
 
+# variable to keep track if a broken symlink was found
+broken_symlink_found=false
+
 # find all symlinks in the specified directory and its subdirectories
 for file in $(find "$scan_dir" -type l); do
-    echo "Symlink found: $file"
-
     # check if the target of the symlink exists
     if [ ! -e "$file" ]; then
-        echo -e "${RED}This file points to a location that does not exist.${NC}"
-    else
-        echo -e "${GREEN}This file points to a valid location.${NC}"
+        echo -e "${RED}Broken symlink found: $file. This file points to a location that does not exist.${NC}"
+        broken_symlink_found=true
     fi
 done
+
+# if no broken symlink was found, print everything is ok
+if [ "$broken_symlink_found" = false ]; then
+    echo -e "${GREEN}Everything is Oke.${NC}"
+fi
